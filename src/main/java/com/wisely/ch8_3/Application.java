66540d195df00fae313b6211c5157ec02ce2a2a9
Mask <2421697126@qq.com>
2018-11-22 18:52:19 +0800

    2018-11-22 第八章第三节 Todo:加入AngularJS进行前后端交互

--- src/main/java/com/wisely/ch8_3/Application.java
+++ src/main/java/com/wisely/ch8_3/Application.java
@@ -2,6 +2,7 @@ package com.wisely.ch8_3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +10,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
 }
---
+++ src/main/java/com/wisely/ch8_3/controller/PersonController.java
@@ -0,0 +1,13 @@
+package com.wisely.ch8_3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PersonController {
+    
+    @RequestMapping("/")
+    public String index() {
+    	return "index";
+    }
+}
